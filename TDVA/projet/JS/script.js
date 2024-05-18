0de48960d0c1a4f0e91115437d1430839b48cba2
Valentin Allmnd <valentin.allmnd@gmail.com>
2024-05-18 09:09:42 +0200

    FIX : overview

--- TDVA/projet/JS/script.js
+++ TDVA/projet/JS/script.js
@@ -1,30 +1,63 @@
 const svgIcons = document.querySelectorAll('.svg-icon');
 const goldenBar = document.getElementById('golden-bar');
 const grayArea = document.getElementById('gray-area');
-
-// Select the first SVG icon
-const firstIcon = svgIcons[0];
-
-// Set the first icon as active by default (optional)
-firstIcon.classList.add('active');
+const grayArea2 = document.getElementById('gray-area-2');
 
 svgIcons.forEach(svgIcon => {
+  if (svgIcon.id === 'overview-id') { // Check if it's the overview icon
     svgIcon.addEventListener('click', function () {
-        goldenBar.style.display = 'block';
+      goldenBar.style.display = 'block';
 
-        // Calculate the offset left position of the clicked SVG icon
-        const offsetLeft = svgIcon.offsetLeft;
+      // Calculate the offset left position of the clicked SVG icon
+      const offsetLeft = svgIcon.offsetLeft;
 
-        // Set the left position of the golden bar to match the clicked SVG icon
-        goldenBar.style.left = `${offsetLeft}px`;
+      // Set the left position of the golden bar to match the clicked SVG icon
+      goldenBar.style.left = `${offsetLeft}px`;
 
-        // Add active class to the corresponding section
-        const sectionId = this.id.replace('id', ''); // Get section ID
-        const sectionElement = document.getElementById(sectionId + '-section');
-        /*if (sectionElement) {
-          sectionElement.classList.add('active');
-          grayArea.classList.add('active'); // Activate gray area
-        }*/
+      // Add active class to the corresponding section
+      const sectionId = this.id.replace('id', ''); // Get section ID
+      const sectionElement = document.getElementById(sectionId + '-section');
+
+      // Display the gray area
+      grayArea.style.opacity = 1;
+      // Hide the gray area 2
+      grayArea2.style.opacity = 0;
 
     });
+  }else if (svgIcon.id === 'monitor-id') {
+    svgIcon.addEventListener('click', function () {
+      goldenBar.style.display = 'block';
+
+      const offsetLeft = svgIcon.offsetLeft;
+
+      goldenBar.style.left = `${offsetLeft}px`;
+      const sectionId = this.id.replace('id', ''); // Get section ID
+      const sectionElement = document.getElementById(sectionId + '-section');
+      grayArea2.style.opacity = 1;
+      grayArea.style.opacity = 0;
+
+    });
+  } else {
+    svgIcon.addEventListener('click', function () {
+      goldenBar.style.display = 'block';
+
+      const offsetLeft = svgIcon.offsetLeft;
+
+      goldenBar.style.left = `${offsetLeft}px`;
+
+      const sectionId = this.id.replace('id', ''); // Get section ID
+      const sectionElement = document.getElementById(sectionId + '-section');
+
+      grayArea.style.opacity = 0;
+      grayArea2.style.opacity = 0;
+
+    });
+  }
 });
+
+function selectOverview() {
+  const overviewIcon = document.getElementById('overview-id');
+  overviewIcon.dispatchEvent(new Event('click'));
+}
+
+window.addEventListener('load', selectOverview);
