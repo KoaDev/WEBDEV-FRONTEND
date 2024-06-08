9017438b8d95d96b924fb5ee440f2a72bb61a4fe
Femure <maxime.femery@gmail.com>
2024-06-08 18:31:51 +0200

    VF

--- MAES/PROJET/main.js
+++ MAES/PROJET/main.js
@@ -1,11 +1,65 @@
+const svgIcon = document.querySelector('.svg-icon');
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cockpitButton = document.getElementById('cockpit-button');
+    const containerOverview = document.getElementById('containerOverview');
+    const cockpit = document.getElementById('cockpit');
+    const location = document.getElementById('location');
+    const closeButtons = document.querySelectorAll('.close-section');
+
+    closeButtons.forEach(closeButton => {
+        closeButton.style.display = "none";
+    });
+
+    cockpitButton.addEventListener('click', function () {
+        containerOverview.classList.add('active');
+        location.classList.add('active');
+        cockpit.classList.add('active');
+        cockpitButton.style.display = "none";
+        closeButtons.forEach(closeButton => {
+            closeButton.style.display = "flex";
+        });
+
+        goldenBar.style.left = `${svgIcon.offsetLeft}px`;
+        goldenBar.style.top = `${svgIcon.offsetTop + svgIcon.offsetHeight}px`;
+    });
+
+    closeButtons.forEach(closeButton => {
+        closeButton.addEventListener('click', function () {
+            containerOverview.classList.remove('active');
+            location.classList.remove('active');
+            cockpit.classList.remove('active');
+            cockpitButton.style.display = "block";
+            closeButton.style.display = "none";
+
+            goldenBar.style.left = `${svgIcon.offsetLeft}px`;
+            goldenBar.style.top = `${svgIcon.offsetTop + svgIcon.offsetHeight}px`;
+        })
+    });
+});
+
 function toggleDetails(event, id) {
     event.preventDefault();
     var details = document.getElementById(id);
     if (details.style.display === "none" || details.style.display === "") {
         details.style.display = "block";
-        event.target.innerHTML = 'Close <i class="fas fa-chevron-up"></i>';
+        if(id === 'delivery-details'){
+            document.getElementById('arrowDelivery').setAttribute('src', '../../TMAX/projet/svg/ico_arrow_up_hover.svg');
+        }
+        else{
+            document.getElementById('arrowAdmin').setAttribute('src', '../../TMAX/projet/svg/ico_arrow_up_hover.svg');
+        }
+        goldenBar.style.left = `${svgIcon.offsetLeft}px`;
+        goldenBar.style.top = `${svgIcon.offsetTop + svgIcon.offsetHeight}px`;
     } else {
         details.style.display = "none";
-        event.target.innerHTML = 'View more <i class="fas fa-chevron-down"></i>';
+        if(id === 'delivery-details'){
+            document.getElementById('arrowDelivery').setAttribute('src', '../../TMAX/projet/svg/ico_arrow_down_hover.svg');
+        }
+        else{
+            document.getElementById('arrowAdmin').setAttribute('src', '../../TMAX/projet/svg/ico_arrow_down_hover.svg');
+        }
+        goldenBar.style.left = `${svgIcon.offsetLeft}px`;
+        goldenBar.style.top = `${svgIcon.offsetTop + svgIcon.offsetHeight}px`;
     }
 }
--- TDVA/projet/JS/script.js
+++ TDVA/projet/JS/script.js
@@ -3,46 +3,33 @@ const goldenBar = document.getElementById('golden-bar');
 const grayArea = document.getElementById('gray-area');
 const grayArea2 = document.getElementById('gray-area-2');
 
+
 svgIcons.forEach(svgIcon => {
   if (svgIcon.id === 'overview-id') { // Check if it's the overview icon
-  
+
     svgIcon.addEventListener('click', function () {
       goldenBar.style.display = 'block';
 
-      const offsetLeft = svgIcon.offsetLeft;
-      const offsetTop = svgIcon.offsetTop;
-      const iconHeight = svgIcon.offsetHeight; // Obtenir la hauteur de l'icône
-
-      goldenBar.style.left = `${offsetLeft}px`;
-      goldenBar.style.top = `${offsetTop + iconHeight}px`;
-
-      const sectionId = this.id.replace('id', ''); // Get section ID
-      const sectionElement = document.getElementById(sectionId + '-section');
+      goldenBar.style.left = `${svgIcon.offsetLeft}px`;
+      goldenBar.style.top = `${svgIcon.offsetTop + svgIcon.offsetHeight}px`;
 
       // Display the gray area
-      grayArea.style.opacity = 1;
+      grayArea.style.display = "block";
       // Hide the gray area 2
-      grayArea2.style.opacity = 0;
+      grayArea2.style.display = "none";
       grayArea.style.pointerEvents = 'auto';
       grayArea2.style.pointerEvents = 'none';
-      
+
     });
-  }else if (svgIcon.id === 'monitor-id') {
+  } else if (svgIcon.id === 'monitor-id') {
     svgIcon.addEventListener('click', function () {
       goldenBar.style.display = 'block';
 
-      const offsetLeft = svgIcon.offsetLeft;
-      const offsetTop = svgIcon.offsetTop;
-      const iconHeight = svgIcon.offsetHeight; // Obtenir la hauteur de l'icône
+      goldenBar.style.left = `${svgIcon.offsetLeft}px`;
+      goldenBar.style.top = `${svgIcon.offsetTop + svgIcon.offsetHeight}px`;
 
-      goldenBar.style.left = `${offsetLeft}px`;
-      goldenBar.style.top = `${offsetTop + iconHeight}px`;
-
-      const sectionId = this.id.replace('id', ''); // Get section ID
-      const sectionElement = document.getElementById(sectionId + '-section');
-
-      grayArea2.style.opacity = 1;
-      grayArea.style.opacity = 0;
+      grayArea2.style.display = "block";
+      grayArea.style.display = "none";
       grayArea.style.pointerEvents = 'none';
       grayArea2.style.pointerEvents = 'auto';
 
@@ -51,18 +38,11 @@ svgIcons.forEach(svgIcon => {
     svgIcon.addEventListener('click', function () {
       goldenBar.style.display = 'block';
 
-      const offsetLeft = svgIcon.offsetLeft;
-      const offsetTop = svgIcon.offsetTop;
-      const iconHeight = svgIcon.offsetHeight; // Obtenir la hauteur de l'icône
+      goldenBar.style.left = `${svgIcon.offsetLeft}px`;
+      goldenBar.style.top = `${svgIcon.offsetTop + svgIcon.offsetHeight}px`;
 
-      goldenBar.style.left = `${offsetLeft}px`;
-      goldenBar.style.top = `${offsetTop + iconHeight}px`;
-
-      const sectionId = this.id.replace('id', ''); // Get section ID
-      const sectionElement = document.getElementById(sectionId + '-section');
-
-      grayArea.style.opacity = 0;
-      grayArea2.style.opacity = 0;
+      grayArea.style.display = "none";
+      grayArea2.style.display = "none";
 
     });
   }
@@ -78,15 +58,15 @@ function hideSection2() {
   var section2 = document.getElementById("containerOverview");
   var section3 = document.getElementById("golden-bar");
   var section4 = document.getElementById("cockpit-title-id");
- 
-  
-  
+
+
+
   if (section) {
-      section.style.display = 'none';
-  
-      section2.style.display = 'none';
-      section3.style.display = 'none';
-      section4.style.display = 'none';
+    section.style.display = 'none';
+
+    section2.style.display = 'none';
+    section3.style.display = 'none';
+    section4.style.display = 'none';
   }
 }
 
@@ -96,20 +76,18 @@ function hideSection() {
   var section2 = document.getElementById("containerOverview");
   var section3 = document.getElementById("golden-bar");
   var section4 = document.getElementById("cockpit-title-id");
- 
-  
-  
+
+
+
   if (section) {
-      section.style.display = 'none';
-  
-      section2.style.display = 'none';
-      section3.style.display = 'none';
-      section4.style.display = 'none';
-      grayArea.style.pointerEvents = 'none';
-      grayArea2.style.pointerEvents = 'none';
+    section.style.display = 'none';
+
+    section2.style.display = 'none';
+    section3.style.display = 'none';
+    section4.style.display = 'none';
+    grayArea.style.pointerEvents = 'none';
+    grayArea2.style.pointerEvents = 'none';
   }
 }
-
-
 
 window.addEventListener('load', selectOverview);
